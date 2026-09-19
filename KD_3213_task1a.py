9bd3b26cb33623331dc3cbfd05f9c6b7a422d39d
abya-r <abyarao49@gmail.com>
2026-09-20 00:43:49 +0530

    Implement grid generation and survivor isolation functions
    Added functions to generate grid intersections and isolate survivors from an image, including color masking for red and yellow shapes.

--- KD_3213_task1a.py
+++ KD_3213_task1a.py
@@ -47,11 +47,62 @@ def generate_grid_intersections():
 
 # ABYA'S PART
 
-def isolate_survivors(rectified_img):
-    # add code
+def generate_grid_intersections():
+    intersections = {}
+    cell_size = 900.0 / 12.0  # 75.0 pixels per cell
+    cols = [chr(ord('A') + i) for i in range(11)]  # Columns A to K
 
-def compute_centroid(contour):
-    # add code
+    for row_idx in range(1, 12):
+        for col_idx in range(11):
+            x = (col_idx + 1) * cell_size
+            y = row_idx * cell_size
+            label = f"{cols[col_idx]}{row_idx}"
+            intersections[label] = (x, y)
+
+    return intersections
+
+
+def isolate_survivors(rectified_img):
+    hsv = cv2.cvtColor(rectified_img, cv2.COLOR_BGR2HSV)
+
+    lower_red1 = np.array([0, 100, 100])
+    upper_red1 = np.array([10, 255, 255])
+    lower_red2 = np.array([170, 100, 100])
+    upper_red2 = np.array([180, 255, 255])
+
+    mask_red1 = cv2.inRange(hsv, lower_red1, upper_red1)
+    mask_red2 = cv2.inRange(hsv, lower_red2, upper_red2)
+    mask_red = cv2.bitwise_or(mask_red1, mask_red2)
+
+    lower_yellow = np.array([20, 100, 100])
+    upper_yellow = np.array([35, 255, 255])
+    mask_yellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
+
+    raw_red_contours, _ = cv2.findContours(mask_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    raw_yellow_contours, _ = cv2.findContours(mask_yellow, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    red_triangles = []
+    
+    for cnt in raw_red_contours:
+        area = cv2.contourArea(cnt)
+        if area > 40:
+            perimeter = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.04 * perimeter, True)
+            if len(approx) == 3:
+                red_triangles.append(cnt)
+
+    yellow_circles = []
+    
+    for cnt in raw_yellow_contours:
+        area = cv2.contourArea(cnt)
+        if area > 40:  # Noise threshold
+            perimeter = cv2.arcLength(cnt, True)
+            if perimeter > 0:
+                circularity = 4 * np.pi * (area / (perimeter * perimeter))
+                if circularity > 0.7:
+                    yellow_circles.append(cnt)
+
+    return red_triangles, yellow_circles
 
 # KRITIKA'S PART
 
@@ -59,4 +110,3 @@ def render_debug_composite(rectified_img, intersections, red_data, yellow_data):
     # add code
 
 # MAIN PIPELINE EXECUTION ENGINE
-
