2dda1b51ec4e57544096a02d71a44d8e6a325ce5
abya-r <abyarao49@gmail.com>
2026-09-19 23:11:05 +0530

    modified boiler plate

--- KD_3213_task1a.py
+++ KD_3213_task1a.py
@@ -15,7 +15,26 @@ def load_image_and_validate(image_path):
 def detect_aruco_markers(img):
     # add code
 
-
 # ADITRI'S PART
 
+def straighten_arena(img, marker_corners):
+    # add code
+
+def generate_grid_intersections():
+    # add code
+
+# ABYA'S PART
+
+def isolate_survivors(rectified_img):
+    # add code
+
+def compute_centroid(contour):
+    # add code
+
+# KRITIKA'S PART
+
+def render_debug_composite(rectified_img, intersections, red_data, yellow_data):
+    # add code
+
+# MAIN PIPELINE EXECUTION ENGINE
 
