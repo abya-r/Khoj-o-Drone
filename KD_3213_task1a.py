117b6c1d617a453d778d11de769f0d95277d7a4f
abya-r <abyarao49@gmail.com>
2026-09-19 22:43:23 +0530

    adding boiler plate

--- KD_3213_task1a.py
+++ KD_3213_task1a.py
@@ -1,1 +1,21 @@
+import cv2
+import numpy as np
+import argparse
+import os
+import sys
+
+# ANAMIKA'S PART
+
+def parse_args():
+    # add code
+
+def load_image_and_validate(image_path):
+    # add code
+
+def detect_aruco_markers(img):
+    # add code
+
+
+# ADITRI'S PART
+
 
